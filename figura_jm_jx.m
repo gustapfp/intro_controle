4efc666ab45d73f7aacac0b3c8ec35c2e807931d
gustapfp <63250479+gustapfp@users.noreply.github.com>
2021-02-03 10:12:11 -0300

    Update figura_jm_jx.m
    gera os vetores jx e jm e plota as figuras  um do lado do outro e gera

--- figura_jm_jx.m
+++ figura_jm_jx.m
@@ -1,11 +1,39 @@
-function[figurajs] = fig_jx_jm (jx, jm)
+function[jx_jm] = fig_jx_jm (gx, sx, px, gm, sm, pm)
+
+% i = > nome da variavel indexadora
+% gx = > g é o valor de i na primeira iteração 
+% sx = > s é o passo ou incremento em i após cada iteração 
+% px = > p é o valor de i na última iteração
+
+% gm = > gm é o valor de i na primeira iteração 
+% sm = > sm é o passo ou incremento em i após cada iteração 
+% pm = > pm é o valor de i na última iteração
+
+% valores de x
+index_jx = 1; % variavel indexadoraauxiliar para iteração do vetor Jx
+jx = [];
+
+for i = gx:sx:px
+    jx(index_jx) = custo_producao(1, i);
+    index_jx = index_jx +1
+  
+end
+
+jm = [];
+index_jm = 1; % variavel indexadoraauxiliar para iteração do vetor Jm
+for i = gm:sm:pm
+    jm(index_jm) = custo_producao(i, 1);
+    index_jm = index_jm +1
+  
+end
+% valores de m 
 
 figure; 
 
 % figura jx
 subplot(1,2,1);
-plot(x, jx);
+plot(gx:sx:px, jx);
 
 %figura jm
 subplot(1, 2, 2)
-plot(m, jm)
+plot(gm:sm:pm, jm)
